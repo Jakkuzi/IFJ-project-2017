006074f69858e9a25857fa77f0c349d0cecd7dd1
TheEzo <willaschek.t@gmail.com>
2017-11-26 12:23:20 +0100

    repair push from wrong branch

--- symtable.h
+++ symtable.h
@@ -1,17 +1,16 @@
 /************************************************************************/
-/*      Toto je header pro soubor symtable.c	 	        	*/
-/*      Zde budou includovany knihovny, definice datovych typu		*/
-/*	a prototypy funkci pro praci s binarnimi vyhledavacimi 		*/
-/*	stromy.								*/
+/*      Toto je header pro soubor symtable.c	 	                  	*/
+/*      Zde budou includovany knihovny, definice datovych typu	    	*/
+/*	    a prototypy funkci pro praci s binarnimi vyhledavacimi 	       	*/
+/*	    stromy.					                		            	*/
 /************************************************************************/
 
 #ifndef SYMTABLE_H
 #define SYMTABLE_H
 
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
-#include<stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 #define STACK_MAX 1000	// maximalni velikost pomocneho zasobniku
 
@@ -24,50 +23,53 @@ typedef struct BTNode BTNode;   // deklarace kvuli pouziti pred definici (pouzit
 
 // datove typy promennych
 typedef enum {
-	var_integer,
-	var_double,
-	var_string,
-	var_bool,
-	var_void
+    var_integer,
+    var_double,
+    var_string,
+    var_bool,
+    var_void
 } varDataType;
 
 // typ polozky BVS (promenna nebo funkce)
 typedef enum {
-	item_type_variable,
-	item_type_function
+    item_type_variable,
+    item_type_function
 } itemType;
 
 // struktura popisujici data promenne
 typedef struct varData {
-        varDataType type;       // datovy typ promenne
-        void* data;             // efektivni (uzitecna) data, pretypovani ukazatele podle datoveho typu promenne
+    varDataType type;       // datovy typ promenne
+    void* data;             // efektivni (uzitecna) data, pretypovani ukazatele podle datoveho typu promenne
 } *varDataPtr;
 
 // struktura popisujici data funkce
 typedef struct funcData {
-        varDataType returnType; // datovy typ navratove hodnoty promenne
-	BTNode **ParamRootPtr;	// ukazatel (na ukazatel) na koren binarniho stromu parametru funkce
+    varDataType returnType; // datovy typ navratove hodnoty promenne
+    BTNode *ParamRootPtr;	// ukazatel  na koren binarniho stromu parametru funkce
 } *funcDataPtr;
 
 // struktura popisujici jednu promennou
 typedef struct BTItem {
-	char* itemID;		// klic, podle ktereho bude uzel vyhledavan (identifikator promenne)
-	itemType itemType;	// typ polozky (promenna nebo funkce)
-	varDataPtr varData;	// ukazatel na data promenne (pokud je itemType = item_type_variable)
-	funcDataPtr funcData;	// ukazatel na data funkce (pokud je itemType = item_type_function)
-} BTItem;
+    char* itemID;		// klic, podle ktereho bude uzel vyhledavan (identifikator promenne)
+    itemType itemType;	// typ polozky (promenna nebo funkce)
+    varDataPtr varData;	// ukazatel na data promenne (pokud je itemType = item_type_variable)
+    funcDataPtr funcData;	// ukazatel na data funkce (pokud je itemType = item_type_function)
+    int declared;
+    int defined;
+    int paramCount;
+} BTItemPtr;
 
 // jeden uzel binarniho vyhledavaciho stromu (BVS)
 typedef struct BTNode {
-	BTItem item;		// struktura popisujici uzitecnou hodnotu (promennou)
-	struct BTNode *LPtr;	// ukazatel na levy podstrom
-        struct BTNode *RPtr;	// ukazatel na pravy podstrom
+    BTItemPtr *item;		// struktura popisujici uzitecnou hodnotu (promennou)
+    struct BTNode *LPtr;	// ukazatel na levy podstrom
+    struct BTNode *RPtr;	// ukazatel na pravy podstrom
 } *BTNodePtr;		// ukazatel na uzel
 
 // pomocny zasobnik pro uchovani ukazatelu na uzly
 typedef struct {
-        BTNodePtr st[STACK_MAX];
-        int top;
+    BTNodePtr st[STACK_MAX];
+    int top;
 } BTStack;
 
 
@@ -78,26 +80,26 @@ typedef struct {
 /* navratove typy (bool) nekterych nasledujicich funkci slouzi pro zjisteni spravnosti provedeni funkce */
 
 /// Vnitrni funkce ///
-void ErrMessage(int, char *);
-bool BTInsert(BTNodePtr *, BTItem * );		// vlozeni nove polozky do stromu, predani struktury ukazatelem (efektivnejsi)
+int BTInsert(BTNodePtr, BTItemPtr * );		// vlozeni nove polozky do stromu, predani struktury ukazatelem (efektivnejsi)
 
 void BTStackInit(BTStack *);			// inizializace pomocneho zasobniku ukazatelu
-bool BTStackPush(BTStack *, BTNodePtr); 	// vlozeni ukazatele na vrchol zasobniku
+int BTStackPush(BTStack *, BTNodePtr); 	// vlozeni ukazatele na vrchol zasobniku
 BTNodePtr BTStackPop(BTStack *);		// precteni vrcholu zasobniku a snizeni vrcholu zasobniku
 
 
 /// API rozhrani - funkce, ktere pouzivejte ///
-void BTInit(BTNodePtr *);                       // inicializace stromu, parametrem je ukazatel na
-bool BTSearch(BTNodePtr, char *, BTItem *);	// najde polozku podle identifikatoru (char *)
-						// v BVS (BTNodePtr) a vrati ji pres ukazatel (BTItem *)
-bool BTInsertVarInt(BTNodePtr *, char *, int);		// vlozi integer
-bool BTInsertVarDouble(BTNodePtr *, char *, double);	// vlozi double
-bool BTInsertVarString(BTNodePtr *, char *, char *);	// vlozi string
+void BTInit(BTNodePtr);                       // inicializace stromu, parametrem je ukazatel na
+BTItemPtr *BTSearch(BTNodePtr, char *);	// najde polozku podle identifikatoru (char *)
+// v BVS (BTNodePtr) a vrati ji pres ukazatel (BTItem *)
+int BTInsertVarInt(funcDataPtr, char *, int);		// vlozi integer
+int BTInsertVarDouble(funcDataPtr , char *, double);	// vlozi double
+int BTInsertVarString(funcDataPtr , char *, char *);	// vlozi string
 
-bool BTInsertFunc(BTNodePtr *, varDataType, char *, BTNodePtr *);	// vlozi funkci
+
+int BTInsertFunc(BTNodePtr, varDataType, char *);	// vlozi funkci
 
 void BTDispose(BTNodePtr *);			// nerekurzivne zrusi cely strom (uvolni alokovanou pamet)
-						// vyuziva zasobniku ukazatelu BTStack
+// vyuziva zasobniku ukazatelu BTStack
 
 
 #endif
--- syntax_check.c
+++ syntax_check.c
@@ -116,6 +116,10 @@ int ll[21][8][8] = {
 
 /* syntax analysis top down */
 int syntax_analysis(tCodeList *C){
+    //symtable TODO: uvolnit symBTree pri chybe
+    BTNodePtr symBTree = (struct BTNode *) malloc(sizeof(struct BTNode));
+    BTItemPtr *actualFunction;
+
     tStack *s;
     s = (tStack *) malloc(sizeof(tStack));
     if(s == NULL)
@@ -284,6 +288,129 @@ int syntax_analysis(tCodeList *C){
         }
         else{
             if(t == EndOfLine){
+                /* SEMANTIC CHECK
+                 * filling symtable from here
+                 *TODO: než se vloží nový řádek
+                 * načíst aktuální data do symtable --- z C->last
+                 * zkontrolovat symtable, jestli je zatím správně
+                 * symBTree - strom plny ID funkci
+                 *
+                 * */
+                int id = C->last->lineData->tokenID;
+                char *name; // name of function or variable
+                varDataType idReturnType;
+                tLinePtr tmp = C->last->lineData; // iterator to get exact data
+                // create function in symtable
+                if(id == Declare || id == Function || id == Scope){
+                    while(tmp->tokenID != RightParenthes)
+                        tmp = tmp->next;
+                    // first next is As, second is data type, so tmp contains data type now
+                    tmp = tmp->next->next;
+                    switch (tmp->tokenID){
+                        case Double:
+                            idReturnType = var_double;
+                            break;
+                        case Integer:
+                            idReturnType = var_integer;
+                            break;
+                        case String:
+                            idReturnType = var_string;
+                            break;
+                        default:
+                            return 2;
+                    }
+                    switch (id){
+                        case Declare:
+                            name = C->last->lineData->next->next->token->myString;
+
+                            result = BTInsertFunc(symBTree, idReturnType, name);
+                            if(result != 0)//TODO: free some shits
+                                return result;
+                            actualFunction = BTSearch(symBTree, name);
+                            actualFunction->declared += 1;
+                            break;
+                        case Function:
+                            name = C->last->lineData->next->token->myString;
+                            result = BTInsertFunc(symBTree, idReturnType, name);
+                            if(result != 0)//TODO: free some shits
+                                return result;
+                            actualFunction = BTSearch(symBTree, name);
+                            actualFunction->defined += 1;
+                            // definition is also declaration
+                            if(actualFunction->declared == 0)
+                                actualFunction->declared = 1;
+                            break;
+                        case Scope:
+                            name = (char *) malloc(sizeof(char) * 20);
+                            strcmp(name, SCOPE_NAME);
+                            result = BTInsertFunc(symBTree, idReturnType, name);
+                            actualFunction = BTSearch(symBTree, name);
+                            actualFunction->declared = 1;
+                            actualFunction->defined = 1;
+                            break;
+                        default: //should not been reachable
+                            return 2;
+                    }
+                    //insert parameters from function
+                    tmp = C->last->lineData;
+                    while(tmp->tokenID != LeftParenthes || tmp == NULL)
+                        tmp = tmp->next;
+                    //tmp is on ID or ')' position
+                    tmp = tmp->next;
+                    while(tmp->tokenID != RightParenthes){
+                        name = tmp->token->myString;
+                        switch (tmp->next->next->tokenID){
+                            case Double:
+                                idReturnType = var_double;
+                                break;
+                            case Integer:
+                                idReturnType = var_integer;
+                                break;
+                            case String:
+                                idReturnType = var_string;
+                                break;
+                            default:
+                                return 2;
+                        }
+                        //TODO: je potreba vkladat hodnoty???
+                        switch (idReturnType){
+                            case var_integer:
+                                result = BTInsertVarInt(actualFunction->funcData, name, 0);
+                                if(result != 0)
+                                    return result;
+                                break;
+                            case var_string:
+                                result = BTInsertVarString(actualFunction->funcData, name, name);
+                                if(result != 0)
+                                    return result;
+                                break;
+                            case var_double:
+                                result = BTInsertVarDouble(actualFunction->funcData, name, 0.0);
+                                if(result != 0)
+                                    return result;
+                                break;
+                        }
+                        actualFunction->paramCount += 1;
+                        tmp = tmp->next->next->next;
+                    }
+
+                }
+                else{ // insert variables from function to symBTree
+
+
+                }
+
+                /* symtable filled */
+
+                //TODO: zkontroluj kazdy radek, jestli je zatim spravne
+                // symTree je ukazatel na root
+                // actualFunction je funkce, ve ktere preva pracujeme
+                // search je vhodne aplikovat prvne na funkci pak prekontrolovat,
+                // jeslti se promenna nejmenuje jako funkce
+                // GL :-)
+
+
+                /* end of SEMANTIC CHECK */
                 result = tCodeCreateNewLine(C);
                 if(result != 0){
                     freeThisCycle(token, s);
